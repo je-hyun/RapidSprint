7c9c996e127c6df61d07a6a863cd7688010ffd33
Je Hyun Kim <jhynkm@gmail.com>
2019-12-04 16:47:21 -0500

    Initial release, add edit delete list

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,28 @@
 
 # import flask_wtf
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, IntegerField
 from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms.fields.html5 import DateField, TimeField
+import datetime
 
 
-
+class SearchForm(FlaskForm):
+    searchbox = StringField('Search', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
 class TaskForm(FlaskForm):
     task_desc = StringField('task_desc', validators=[DataRequired()])
     task_status_completed = SelectField('Status', choices=[('todo','Todo'),('doing','Doing'),('done','Done')])
     submit = SubmitField('submit')
+
+class AppointmentForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()], default="My Appointment Title")
+    start_date = DateField('Start Date', validators=[DataRequired()], default=datetime.datetime.now().date())
+    start_time = TimeField('Start Time', validators=[DataRequired()], default=datetime.datetime.now().time())
+    duration = IntegerField('Duration (In minutes)', validators=[DataRequired()], default=60)
+    location = StringField('Location Address',validators=[DataRequired()], default="8000 Utopia Parkway")
+    customer_name = StringField('Customer Name',validators=[DataRequired()], default="Je Hyun Kim")
+    notes = StringField('Notes',validators=[DataRequired()], default="Remember to bring folder!")
+    status = StringField('Status',validators=[DataRequired()], default="incomplete")
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -10,3 +10,13 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     task_desc = db.Column(db.String(128), index=True)
     task_status = db.Column(db.String(128))
+
+class Appointment(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    date_time = db.Column(db.DateTime)
+    duration = db.Column(db.Integer)
+    location = db.Column(db.String)
+    customer_name = db.Column(db.String)
+    notes = db.Column(db.String)
+    status = db.Column(db.String)
